37df0ab08c607eb50ecf1682f1b99b2c64c50965
ajpf44 <ajpf44@gmail.com>
2024-05-23 15:00:14 -0300

    Refactor: melhorias gerais no código

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,42 @@
-import React, { useContext } from 'react';
-import { getAllUsers } from '../../services/users';
-import { SessionContext } from '../../contexts/SessionContext';
+import React from "react";
+import { getAllUsers } from "../../services/users";
+import useSession from "../../hooks/useSession";
+import { Box } from "@mui/material";
+import ButtonFilled from "../../components/Button/ButtonFilled";
 
 const Home: React.FC = () => {
-  
-  const sessionCtx = useContext(SessionContext);
+  const session = useSession();
 
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "89.2vh",
+        gap: "1rem",
+        fontSize: "1.2rem",
+        backgroundColor: "primary.main",
+        color: "primary.contrastText",
+        textAlign: "center",
+      }}
+    >
       <h1>Projeto React Web - Ecommerce</h1>
 
-      {sessionCtx.user?.name ? <p>Olá usuário {sessionCtx.user?.name}</p>: ""}
-      
-      <button
-        onClick={()=>getAllUsers()}
-      >Console usuários</button>
-      <p>Como importa no váriaveis de ambiente no código: {import.meta.env.VITE_foo_var}</p>
-    </div>
+      {session.user?.name ? <p>Olá usuário {session.user?.name}</p> : ""}
+
+      <div>
+        <ButtonFilled onClick={() => getAllUsers()}>
+          {" "}
+          Console usuários{" "}
+        </ButtonFilled>
+      </div>
+      <p>
+        Como importa no váriaveis de ambiente no código:{" "}
+        {import.meta.env.VITE_foo_var}
+      </p>
+    </Box>
   );
 };
 
--- src/types/ProductsTypes.d.ts
+++ src/types/ProductsTypes.d.ts
@@ -1,14 +1,14 @@
 type ProductType = {
-  category: "smartphones";
-  description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...";
-  discountPercentage: '17.94';
-  id: '2';
+  category: string;
+  description: string;
+  discountPercentage: string;
+  id: string;
   images: string[];
-  price: '899';
-  rating: '4.44';
-  stock: '34';
-  thumbnail: "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg";
-  title: "iPhone X";
+  price: string;
+  rating: string;
+  stock: string;
+  thumbnail: string;
+  title: string;
 };
 
 export { ProductType };

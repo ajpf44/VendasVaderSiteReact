2f7d9d380062db034880b2e89e7d109aca67a063
Rodrigo costa <rodrigo.dev.deb@gmail.com>
2024-05-25 10:58:41 -0300

    implementação buttonSwitch para mudança tema

--- src/components/AccountMenu/AccountMenu.tsx
+++ src/components/AccountMenu/AccountMenu.tsx
@@ -1,17 +1,18 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
+import * as React from "react";
+
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
 import { SessionContext } from "../../contexts/SessionContext";
 import { useContext } from "react";
+import CustomizedSwitches from "../Button/ButtonSwitch";
 
 export default function AccountMenu() {
   const { logout } = useContext(SessionContext);
@@ -38,9 +39,9 @@ export default function AccountMenu() {
           onClick={handleClick}
           size="small"
           sx={{ ml: 2 }}
-          aria-controls={open ? 'account-menu' : undefined}
+          aria-controls={open ? "account-menu" : undefined}
           aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
+          aria-expanded={open ? "true" : undefined}
         >
           <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
         </IconButton>
@@ -50,35 +51,35 @@ export default function AccountMenu() {
         id="account-menu"
         open={open}
         onClose={handleClose}
-        onClick={handleClose}
+      //  onClick={handleClose}
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&::before': {
+            "&::before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         <MenuItem onClick={handleClose}>
           <Avatar /> Profile
@@ -93,11 +94,15 @@ export default function AccountMenu() {
           </ListItemIcon>
           Add another account
         </MenuItem>
+        
         <MenuItem onClick={handleClose}>
           <ListItemIcon>
             <Settings fontSize="small" />
           </ListItemIcon>
           Settings
+        </MenuItem>
+        <MenuItem>
+        <CustomizedSwitches />
         </MenuItem>
         <MenuItem onClick={handleLogout}>
           <ListItemIcon>
@@ -105,8 +110,8 @@ export default function AccountMenu() {
           </ListItemIcon>
           Logout
         </MenuItem>
+     
       </Menu>
     </React.Fragment>
   );
 }
-

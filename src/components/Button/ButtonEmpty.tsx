14fc3434c13fa42b5efcb4aa1d2a2e824dae0039
ajpf44 <ajpf44@gmail.com>
2024-05-26 19:21:46 -0300

    Feature: termina a página Home

--- src/components/Button/ButtonEmpty.tsx
+++ src/components/Button/ButtonEmpty.tsx
@@ -4,14 +4,15 @@ import { Button as ButtonMUI } from "@mui/material";
 interface ButtonProps {
   text?: string;
   children: string;
+  color?: "secondary"|"primary"
   onClick?: () => void;
 }
 // >>>>>>>>>>>>>>>>>>  SE FOR MUITO ESPECÍFICO, USE DIRETO DO MUI AO INVEZ DESSE
 // https://mui.com/material-ui/react-button/
 
-const ButtonEmpty: React.FC<ButtonProps> = ({ children, onClick }) => {
+const ButtonEmpty: React.FC<ButtonProps> = ({ children, onClick, color="secondary" }) => {
   return (
-    <ButtonMUI onClick={onClick} variant="outlined" color="secondary" fullWidth>
+    <ButtonMUI onClick={onClick} variant="outlined" color={color} fullWidth>
       {children}
     </ButtonMUI>
     // <button className="beautiful-button" onClick={onClick}>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,52 +1,127 @@
-import React, { useContext } from "react";
-
-import { Box, Typography } from "@mui/material";
+import React from "react";
+import { motion } from "framer-motion";
+import { Box, Link, Typography } from "@mui/material";
 
 import img from "../../assets/backgroundHome.jpg";
 import "./Home.css";
+import ButtonEmpty from "../../components/Button/ButtonEmpty";
+import { Link as LinkRouter } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <Box
       sx={{
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "space-evenly",
         alignItems: "center",
         flexDirection: "column",
         minHeight: "90vh",
-        gap: "1rem",
-        fontSize: "1.2rem",
+
         backgroundImage: `url(${img})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-        color: "primary.contrastText",
-        textAlign: "center",
+        backgroundAttachment: "fixed",
+
+        overflow: "auto",
       }}
     >
-      <Box
-        className="animate"
-        sx={{ background: "rgba(0,0,0,0.5)", width: "100%", height: "90vh" }}
+      <motion.div
+        style={{ width: "100%" }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 4,
+          delay: 0.7,
+          ease: [0, 0.71, 0.2, 1.01],
+          scale: {
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
       >
-        <Typography
-          variant="h1"
-          color="secondary.dark"
-          className="title jediFont"
+        <Box
+          className="animate"
+          sx={{
+            background: "rgba(0,0,0,0.5)",
+            width: "100%",
+            height: "90vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          {" "}
-          vendas vader
-        </Typography>
-        <Typography color={"white"} className="jediFont" margin={1}>
-          Produtos intergaláticos
-        </Typography>
-        <Typography
-          variant="body1"
-          color={"white"}
-          maxWidth="70vw"
-          textAlign="justify"
-          margin="auto"
-        >
-        </Typography>
-      </Box>
+          <Typography
+            variant="h1"
+            color="secondary.dark"
+            className="title jediFont"
+          >
+            vendas vader
+          </Typography>
+          <Typography color={"white"} className="jediFont" margin={1}>
+            Produtos intergaláticos
+          </Typography>
+
+          <Box
+            color={"white"}
+            maxWidth="60rem"
+            textAlign="justify"
+            margin="auto"
+            height={"70%"}
+            justifyContent={"space-evenly"}
+            display={"flex"}
+            flexDirection={"column"}
+            padding={"2rem 0"}
+          >
+            <Typography variant="h4">Bem-vindo ao Vendas Vader!</Typography>
+
+            <Typography fontSize={"1.1rem"}>
+              Esse é o{" "}
+              <Link
+                href="https://github.com/brjoaof/senac-2024-01-react/tree/master/trabalho-final"
+                color="primary"
+                target="_blank"
+              >
+                Projeto final
+              </Link>{" "}
+              da disciplina de React Web do{" "}
+              <Link href="https://serratec.org/" target="_blank">
+                Serratec
+              </Link>{" "}
+              em parceria com o Senac, instruido pelo Professor João Felipe.
+              Desenvolvido ao longo de 9 dias, nosso objetivo era construir um
+              E-commerce com uma interface agradável, integração com uma API e
+              sistema de autenticação, em React com TypeScript. O código pode
+              ser encontrado no{" "}
+              <Link
+                href="https://github.com/ajpf44/e-commerce-react"
+                target="_blank"
+              >
+                Repositório do Github.
+              </Link>
+            </Typography>
+
+            <Typography fontSize={"1.1rem"}>
+              O Vendas Vader é um E-commerce de variedades, onde você encontrará
+              uma ampla gama de produtos para atender às suas necessidades.
+              Explore nossos produtos e aproveite sua jornada de compras
+              conosco!
+            </Typography>
+            <Box display={"flex"} flexDirection={"column"} gap={"1rem"}>
+              <LinkRouter to={"/members"}>
+                <ButtonEmpty color="primary">Quem somos nós?</ButtonEmpty>
+              </LinkRouter>
+              <LinkRouter
+                to={"https://github.com/ajpf44/e-commerce-react"}
+                target="_blank"
+              >
+                <ButtonEmpty color="primary">Repositório</ButtonEmpty>
+              </LinkRouter>
+            </Box>
+          </Box>
+          <Box maxWidth={"20vw"} margin={"auto"}></Box>
+        </Box>
+      </motion.div>
     </Box>
   );
 };

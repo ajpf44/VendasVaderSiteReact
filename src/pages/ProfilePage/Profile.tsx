c32b9c0c3f4b633e60d9153bd8b940e5a9881068
ajpf44 <ajpf44@gmail.com>
2024-05-27 15:02:12 -0300

    Feat: implementa persistência na alteração do usuário em Profile

--- src/pages/ProfilePage/Profile.tsx
+++ src/pages/ProfilePage/Profile.tsx
@@ -1,89 +1,113 @@
-import React, { useState, useContext, ChangeEvent, FormEvent, useEffect } from 'react';
-import { Typography, Box, TextField, Button, Avatar, Container, Paper, IconButton } from '@mui/material';
-import { styled } from '@mui/system';
-import { SessionContext, SessionContextType } from "../../contexts/SessionContext";
-import PersonIcon from '@mui/icons-material/Person';
-import EditIcon from '@mui/icons-material/Edit';
-import UserType from '../../types/UserType';
+import React, {
+  useState,
+  useContext,
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+} from "react";
+import {
+  Typography,
+  Box,
+  TextField,
+  Button,
+  Avatar,
+  Container,
+  Paper,
+  IconButton,
+  CircularProgress,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import {
+  SessionContext,
+  SessionContextType,
+} from "../../contexts/SessionContext";
+import PersonIcon from "@mui/icons-material/Person";
+import EditIcon from "@mui/icons-material/Edit";
+import UserType from "../../types/UserType";
+import useSession from "../../hooks/useSession";
+import { updateUser } from "../../services/users";
 
 const StyledContainer = styled(Container)(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  backgroundColor: 'rgba(0,0,0,0)',
-  color: '#000',
-  minHeight: '100vh',
-  padding: '20px',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: "rgba(0,0,0,0)",
+  color: "#000",
+  minHeight: "100vh",
+  padding: "20px",
 }));
 
 const StyledAvatar = styled(Avatar)(() => ({
-  backgroundColor: '#FF69B4',
+  backgroundColor: "#FF69B4",
   width: 100,
   height: 100,
-  marginBottom: '20px',
-  cursor: 'pointer',
-  position: 'relative',
+  marginBottom: "20px",
+  cursor: "pointer",
+  position: "relative",
 }));
 
 const EditAvatarButton = styled(IconButton)(() => ({
-  position: 'absolute',
+  position: "absolute",
   bottom: 0,
   right: 0,
-  backgroundColor: '#FFF',
-  color: '#FF69B4',
-  '&:hover': {
-    backgroundColor: '#FF1493',
-    color: '#FFF',
+  backgroundColor: "#FFF",
+  color: "#FF69B4",
+  "&:hover": {
+    backgroundColor: "#FF1493",
+    color: "#FFF",
   },
 }));
 
-const StyledForm = styled('form')(() => ({
-  width: '100%',
-  marginTop: '20px',
+const StyledForm = styled("form")(() => ({
+  width: "100%",
+  marginTop: "20px",
 }));
 
 const StyledTextField = styled(TextField)(() => ({
-  marginBottom: '15px',
-  '& label.Mui-focused': {
-    color: '#FF69B4',
+  marginBottom: "15px",
+  "& label.Mui-focused": {
+    color: "#FF69B4",
   },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: '#FF69B4',
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#FF69B4",
   },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: '#FF69B4',
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#FF69B4",
     },
-    '&:hover fieldset': {
-      borderColor: '#FF1493',
+    "&:hover fieldset": {
+      borderColor: "#FF1493",
     },
-    '&.Mui-focused fieldset': {
-      borderColor: '#FF69B4',
+    "&.Mui-focused fieldset": {
+      borderColor: "#FF69B4",
     },
   },
 }));
 
 const StyledButton = styled(Button)(() => ({
-  marginTop: '20px',
-  backgroundColor: '#FF69B4',
-  color: '#FFF',
-  '&:hover': {
-    backgroundColor: '#FF1493',
+  marginTop: "20px",
+  backgroundColor: "#FF69B4",
+  color: "#FFF",
+  "&:hover": {
+    backgroundColor: "#FF1493",
   },
 }));
 
 const ProfilePage: React.FC = () => {
+  const sessionCtx = useSession();
   const { user, setUser } = useContext(SessionContext) as SessionContextType;
 
+  const [updatingUser, setUpdatingUser] = useState(false);
+
   const [newUserInfo, setNewUserInfo] = useState<UserType>({
-    name: '',
-    email: '',
-    address: '',
-    phone: '',
-    bio: '',
-    job: '',
-    company: '',
-    age: ''
+    name: "",
+    email: "",
+    address: "",
+    phone: "",
+    bio: "",
+    job: "",
+    company: "",
+    age: "",
   });
 
   useEffect(() => {
@@ -96,20 +120,39 @@ const ProfilePage: React.FC = () => {
     const { name, value } = e.target;
     setNewUserInfo((prevInfo) => ({
       ...prevInfo,
-      [name]: value
+      [name]: value,
     }));
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setUser(newUserInfo);
-    localStorage.setItem('userInfo', JSON.stringify(newUserInfo));
+
+    setUpdatingUser(true);
+    const userToStorage: UserType = {
+      ...newUserInfo,
+      email: sessionCtx.user?.email ?? "",
+    };
+    setUser(userToStorage);
+    sessionStorage.setItem("userInfo", JSON.stringify(userToStorage));
+    sessionCtx.setUser(userToStorage);
+
+    await updateUser(userToStorage);
+    setUpdatingUser(false);
   };
 
   return (
     <StyledContainer maxWidth="sm">
-      <Paper elevation={3} sx={{ p: 4, width: '100%', textAlign: 'center', backgroundColor: '#FFF', color: '#000' }}>
-        <Box sx={{ position: 'relative', display: 'inline-block' }}>
+      <Paper
+        elevation={3}
+        sx={{
+          p: 4,
+          width: "100%",
+          textAlign: "center",
+          backgroundColor: "#FFF",
+          color: "#000",
+        }}
+      >
+        <Box sx={{ position: "relative", display: "inline-block" }}>
           <StyledAvatar>
             <PersonIcon fontSize="large" />
           </StyledAvatar>
@@ -117,7 +160,11 @@ const ProfilePage: React.FC = () => {
             <EditIcon />
           </EditAvatarButton>
         </Box>
-        <Typography component="h1" variant="h4" sx={{ color: '#000', marginBottom: '20px' }}>
+        <Typography
+          component="h1"
+          variant="h4"
+          sx={{ color: "#000", marginBottom: "20px" }}
+        >
           Profile Information
         </Typography>
         <StyledForm onSubmit={handleSubmit}>
@@ -132,10 +179,13 @@ const ProfilePage: React.FC = () => {
           <StyledTextField
             name="email"
             label="Email"
-            value={newUserInfo.email}
-            onChange={handleChange}
+            value={sessionCtx.user?.email}
+            onChange={() => {}}
             fullWidth
             variant="outlined"
+            InputProps={{
+              readOnly: true,
+            }}
           />
           <StyledTextField
             name="age"
@@ -187,9 +237,15 @@ const ProfilePage: React.FC = () => {
             fullWidth
             variant="outlined"
           />
-          <StyledButton type="submit" fullWidth variant="contained">
-            Save
-          </StyledButton>
+          {updatingUser ? (
+            <StyledButton type="submit" fullWidth variant="contained" disabled>
+              <CircularProgress color="primary" />
+            </StyledButton>
+          ) : (
+            <StyledButton type="submit" fullWidth variant="contained">
+              Save
+            </StyledButton>
+          )}
         </StyledForm>
       </Paper>
     </StyledContainer>
@@ -197,4 +253,3 @@ const ProfilePage: React.FC = () => {
 };
 
 export default ProfilePage;
-
